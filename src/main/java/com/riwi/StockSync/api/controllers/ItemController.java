c7580e8945cca1f6df9a5a395505a1838d0bbbe8
miguelangito <miguelangelespinalospina@gmail.com>
2024-05-07 21:29:03 -0500

    item completo

--- src/main/java/com/riwi/StockSync/api/controllers/ItemController.java
+++ src/main/java/com/riwi/StockSync/api/controllers/ItemController.java
@@ -3,23 +3,25 @@ package com.riwi.StockSync.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.riwi.StockSync.api.dto.request.ItemRequest;
 import com.riwi.StockSync.api.dto.response.ItemResponseCompleteInformation;
-import com.riwi.StockSync.domain.entities.Item;
 import com.riwi.StockSync.infrastructure.services.interfaces.IItemService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping(path = "/items")
 @RequiredArgsConstructor
 public class ItemController {
 
@@ -38,5 +40,15 @@ public class ItemController {
         
         return ResponseEntity.ok(this.iItemService.create(entity));
     }
-    
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ItemResponseCompleteInformation>update(@PathVariable String id, @RequestBody ItemRequest item){
+        return ResponseEntity.ok(this.iItemService.update(item, id));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        this.iItemService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
