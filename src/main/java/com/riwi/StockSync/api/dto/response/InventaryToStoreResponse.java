14754a87ee7a44d4a8533d4f6a87d650ba3cb7b2
Valeria <valehm248@gmail.com>
2024-05-08 19:23:21 -0500

    Actualización

--- src/main/java/com/riwi/StockSync/api/dto/response/InventaryToStoreResponse.java
+++ src/main/java/com/riwi/StockSync/api/dto/response/InventaryToStoreResponse.java
@@ -1,5 +1,22 @@
 package com.riwi.StockSync.api.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class InventaryToStoreResponse {
-    
+
+    private String id;
+    private LocalDate date;
+    private StoreResponse store;
+    private ProductResponse product;
+
 }
